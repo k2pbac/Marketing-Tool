83a6b1c43ac19d9b3eb5c35412a00ea725c040a2
k2pbac <krisbachan123@hotmail.com>
2021-12-04 19:51:52 -0500

    add list of bank items in storybook

--- src/Components/Marketer/BankItem/BankItem.jsx
+++ src/Components/Marketer/BankItem/BankItem.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 
 import "./BankItem.scss";
-const BankItem = ({ bankData = {}, clicked, clickable = false }) => {
+const BankItem = ({ bankData = {}, clicked }) => {
   const [isClicked, setIsClicked] = useState(clicked);
-
   const bankElements = (isClicked && Object.keys(bankData) && (
     <div>
-      <p>{bankData.name}</p>
+      <p>Temp Data Header</p>
       <p>{bankData.address}</p>
     </div>
   )) || <></>;
@@ -14,13 +13,11 @@ const BankItem = ({ bankData = {}, clicked, clickable = false }) => {
   return (
     <div
       onClick={() => {
-        if (clickable) {
-          setIsClicked((prev) => !prev);
-        }
+        setIsClicked((prev) => !prev);
       }}
       className={`bank_item bank_item__hover`}
     >
-      <h1>Bank Branch 1</h1>
+      <h1>Bank of Montreal</h1>
       {bankElements}
     </div>
   );
