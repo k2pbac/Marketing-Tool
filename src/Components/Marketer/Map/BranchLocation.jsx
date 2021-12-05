2bf695c4c9b83076202f65eac405e0621cf83991
k2pbac <krisbachan123@hotmail.com>
2021-12-04 20:59:10 -0500

    add tooltip to map branch locations

--- src/Components/Marketer/Map/BranchLocation.jsx
+++ src/Components/Marketer/Map/BranchLocation.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 
+import "./BranchLocation.scss";
+
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const BranchLocation = ({ text }) => {
   return (
-    <h3>
-      <FontAwesomeIcon icon={faMapMarkerAlt} />
-    </h3>
+    <div className="branch-location">
+      <OverlayTrigger
+        key="top"
+        placement="top"
+        overlay={
+          <Tooltip>
+            Tooltip on <strong>top</strong>.
+          </Tooltip>
+        }
+      >
+        <h3>
+          <FontAwesomeIcon icon={faMapMarkerAlt} />
+        </h3>
+      </OverlayTrigger>
+    </div>
   );
 };
 
